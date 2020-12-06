3942710f0358d9ef94eb560d4d7bc04dcf166e02
biomusic <dannie.fu.888@gmail.com>
2020-12-06 17:10:31 -0500

    Small changes

--- Symbolic Transfer Entropy/f_nste.m
+++ Symbolic Transfer Entropy/f_nste.m
@@ -2,6 +2,7 @@ function [STE, NSTE] = f_nste(data, dim, lag, delta)
 % only applied for bivariate data
 % (c1,c2): c2 -> c0
 % That is, Column to Row
+
 ch=size(data,2); % For 2 signals, ch=2
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -14,7 +15,6 @@ end
 Int_future=INT(1+delta:end,:);
 Int_past=INT(1:end-delta,:);
 
-% compute probability
 % P1 = P[X(n+1),X(n),Y(n)]
 % P2 = P[X(n),Y(n)]
 % P3 = P[X(n+1),X(n),???]
@@ -23,10 +23,16 @@ Int_past=INT(1:end-delta,:);
 % Integer1: Target Future: X(n+1)
 % Integer2: Target Present: X(n)
 % Integer3: Source Present: Y(n)
+
+% Compute STE_YX and H(X_future | X_past) 
 [P1, P2, P3, P4] = f_Integer2prob(Int_future(:,1), Int_past(:,1), Int_past(:,2)); %(XF, XP, YP)
 STE(1) = sum( P1 .* (log2( P1.*P4 ) - log2(P2.*P3)) ); % P1 = P(XF,XP,YP) => target is X, source is Y => STE_YX
+H(1) = -sum ( P3 .* (log2(P3) - log2(P4)));
+
+% Compute STE_XY and H(Y_future | Y_past) 
 [P1, P2, P3, P4] = f_Integer2prob(Int_future(:,2), Int_past(:,2), Int_past(:,1)); %(YF, YP, XP)
 STE(2) = sum( P1 .* (log2( P1.*P4 ) - log2(P2.*P3)) ); % P1 = P(YF,YP,YP) => target is Y, source is X => STE_XY
+H(2) = -sum ( P3 .* (log2(P3) - log2(P4)));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%  Part 2. STE of shuffled data  %%%%%%%%%%%%%%%%%
@@ -37,29 +43,32 @@ STE(2) = sum( P1 .* (log2( P1.*P4 ) - log2(P2.*P3)) ); % P1 = P(YF,YP,YP) => tar
 
 % second data is shuffled because it is source 
 data2=data(:,2); 
-len=length(data2);
-halflen=floor(len/2);
-shuffled_data2 = [data2(halflen+1:end); data2(1:halflen)]; % shuffle Y
 
-Ddata=delayRecons([data(:,1) shuffled_data2], lag, dim);
-clear INT;
-for c=1:ch
-    INT(:,c)=F_Int2Symb(Ddata(:,:,c));
-end
-Int_future=INT(1+delta:end,:);
-Int_past=INT(1:end-delta,:);
+num_trials = 20; % Number of trials for getting shuffled value 
+for i = 1:num_trials
+    shuffled_data2 = data2(randperm(length(data2))); % shuffle data 
+    
+    Ddata=delayRecons([data(:,1) shuffled_data2], lag, dim);
+    clear INT;
+    for c=1:ch
+        INT(:,c)=F_Int2Symb(Ddata(:,:,c));
+    end
+    Int_future=INT(1+delta:end,:);
+    Int_past=INT(1:end-delta,:);
 
-[P1, P2, P3, P4] = f_Integer2prob(Int_future(:,1), Int_past(:,1), Int_past(:,2));
-STE_shuffled = sum( P1 .* (log2( P1.*P4 ) - log2(P2.*P3)) );
-H_XY = -sum ( P3 .* (log2(P3) - log2(P4)));
-
-NSTE_numerator = STE(1) - STE_shuffled; %STE(1) is STE_YX
-
-if NSTE_numerator < 0
-    NSTE(1) = 0; % set NSTE to 0 if shuffled STE is greater than normal STE 
-else
-    NSTE(1) = NSTE_numerator/H_XY;  
+    [P1, P2, P3, P4] = f_Integer2prob(Int_future(:,1), Int_past(:,1), Int_past(:,2));
+    STE_shuffled_all(i) = sum( P1 .* (log2( P1.*P4 ) - log2(P2.*P3)) );
 end 
+
+STE_shuffled_ave = mean(STE_shuffled_all);
+
+NSTE_numerator(1) = STE(1) - STE_shuffled_ave; %STE(1) is STE_YX
+
+% if NSTE_numerator(1) < 0
+%     NSTE(1) = 0; % set NSTE to 0 if shuffled STE is greater than normal STE 
+% else
+    NSTE(1) = NSTE_numerator(1)/H(1);  
+% end 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%  Part 3. STE of shuffled data  %%%%%%%%%%%%%%%%%
@@ -70,29 +79,32 @@ end
 
 % first data is shuffled because it is source 
 data1=data(:,1); % source data
-len=length(data1);
-halflen=floor(len/2);
-shuffled_data1 = [data1(halflen+1:end); data1(1:halflen)];
 
-Ddata=delayRecons([shuffled_data1 data(:,2)], lag, dim);
-clear INT;
-for c=1:ch
-    INT(:,c)=F_Int2Symb(Ddata(:,:,c));
-end
-Int_future=INT(1+delta:end,:);
-Int_past=INT(1:end-delta,:);
+num_trials = 20; % Number of trials for getting shuffled value 
+for i = 1:num_trials
+    shuffled_data1 = data1(randperm(length(data1)));
+    
+    Ddata=delayRecons([shuffled_data1 data(:,2)], lag, dim);
+    clear INT;
+    for c=1:ch
+        INT(:,c)=F_Int2Symb(Ddata(:,:,c));
+    end
+    Int_future=INT(1+delta:end,:);
+    Int_past=INT(1:end-delta,:);
 
-[P1, P2, P3, P4] = f_Integer2prob(Int_future(:,2), Int_past(:,2), Int_past(:,1));
-STE_shuffled = sum( P1 .* (log2( P1.*P4 ) - log2(P2.*P3)) );
-H_XY = -sum ( P3 .* (log2(P3) - log2(P4)));
-
-STE_numerator = STE(2) - STE_shuffled;
-
-if STE_numerator < 0
-    NSTE(2) = 0; % set NSTE to 0 if shuffled STE is greater than normal STE 
-else
-    NSTE(2) = STE_numerator/H_XY; % STE(2) is STE_XY => NSTE(2) is NSTE_XY
+    [P1, P2, P3, P4] = f_Integer2prob(Int_future(:,2), Int_past(:,2), Int_past(:,1));
+    STE_shuffled_all = sum( P1 .* (log2( P1.*P4 ) - log2(P2.*P3)) );
 end 
+
+STE_shuffled_ave = mean(STE_shuffled_all);
+
+NSTE_numerator(2) = STE(2) - STE_shuffled_ave;
+
+% if NSTE_numerator(2) < 0
+%     NSTE(2) = 0; % set NSTE to 0 if shuffled STE is greater than normal STE 
+% else
+    NSTE(2) = NSTE_numerator(2)/H(2); % STE(2) is STE_XY => NSTE(2) is NSTE_XY
+% end 
 
 
 
