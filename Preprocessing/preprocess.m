a8f77382d74d783917002dcf29b86e6b06fc28b2
biomusic <dannie.fu.888@gmail.com>
2021-01-27 14:35:26 -0500

    Including HRVYZ and HRVZ

--- Preprocessing/preprocess.m
+++ Preprocessing/preprocess.m
@@ -1,36 +1,36 @@
-% Dannie Fu June 2 2020
-%
-% This script preprocesses EDA, TEMP, HR, HRVZ/Y
-% Saves the cleaned data in a struct called "clean", with EDA, HR, HRVZY,
-% TEMP
-% ------------------
+function clean = preprocess(EDA,TEMP,HR,HRV)
+% TODO: add description
+% Input - EDA, TEMP, HR  HRV tables
+% Output - EDA, TEMP, HR, HRV cleaned [time data]
 
-%clear;
+% Sometimes the headers in the tables are capitalized or not captilized
+try
+    EDA_time = EDA.eda_time;
+    TEMP_time = TEMP.temp_time;
+    HR_time = HR.hr_time;
+    HRV_time = HRV.hrv_time;
 
-%% This section is for running on one folder - comment out if running on many folders from another script
+    EDA_data = EDA.eda_data;
+    TEMP_data = TEMP.temp_data;
+    HR_data = HR.hr_data;
+    HRV_X = HRV.hrv_x;
+    HRV_Z = HRV.hrv_z;
+    HRV_Y = HRV.hrv_y;
+    HRV_YZ = HRV_Y./HRV_Z;
+catch
+    EDA_time = EDA.EDA_time;
+    TEMP_time = TEMP.TEMP_time;
+    HR_time = HR.HR_time;
+    HRV_time = HRV.HRV_time;
 
-% Input params 
-LOAD_DIR = "/Volumes/Seagate/Codesign/cdnc005/2020-11-18/";
-OUT_DIR = LOAD_DIR;
-
-%Load all the mat files (but will only preprocess temp, eda, hr, hrv)
-files = dir(strcat(LOAD_DIR,'*.mat'));
-for i=1:length(files)
-    filepath = strcat(LOAD_DIR,files(i).name);
-    load(filepath);
-end
-
-%%
-% Sometimes the headers in the tables are capitalized so can change the names here instead of all over the script
-EDA_time = EDA.eda_time;
-TEMP_time = TEMP.temp_time;
-HR_time = HR.hr_time;
-HRV_time = HRV.hrv_time;
-
-EDA_data = EDA.eda_data;
-TEMP_data = TEMP.temp_data;
-HR_data = HR.hr_data;
-HRV_ZY = HRV.hrv_z ./ HRV.hrv_y;
+    EDA_data = EDA.EDA_data;
+    TEMP_data = TEMP.TEMP_data;
+    HR_data = HR.HR_data;
+    HRV_X = HRV.HRV_x;
+    HRV_Z = HRV.HRV_z;
+    HRV_Y = HRV.HRV_y;
+    HRV_YZ = HRV_Y./HRV_Z;
+end 
 
 % Cut beginning if data starts with 0s 
 % Find index where EDA is larger than 0.05 (smaller is invalid data). Finds the first 10 indices because sometimes
@@ -55,24 +55,31 @@ end
 [EDA_data, EDA_time] = unpad(EDA_data,EDA_time, t_start,t_end);
 [TEMP_data, TEMP_time] = unpad(TEMP_data, TEMP_time, t_start,t_end);
 [HR_data, HR_time] = unpad(HR_data, HR_time, t_start,t_end);
-[HRV_ZY, HRV_time] = unpad(HRV_ZY,HRV_time, t_start,t_end);
+[HRV_YZ, HRV_time] = unpad(HRV_YZ,HRV_time, t_start,t_end);
+[HRV_X, HRV_time] = unpad(HRV_X,HRV_time, t_start,t_end);
+[HRV_Y, HRV_time] = unpad(HRV_Y,HRV_time, t_start,t_end);
+[HRV_Z, HRV_time] = unpad(HRV_Z,HRV_time, t_start,t_end);
 
 % Call preprocessing scripts
 [EDA_medfilt, EDA_avefilt,EDA_interp, EDA_eurofilt] = preprocessEDA(EDA_data);
 [TEMP_medfilt, TEMP_avefilt, TEMP_interp, TEMP_expfilt] = preprocessTEMP(TEMP_data);
-[HR_avefilt, HR_cubic, HRV_ZY_avefilt, HRV_ZY_interp, HRV_ZY_cubic] = preprocessHR(HR_data, HR_time, HRV_ZY, HRV_time);
+[HR_avefilt, HR_cubic, ...
+ HRV_YZ_avefilt, HRV_YZ_interp, HRV_YZ_cubic, ...
+ HRV_X_avefilt, HRV_X_interp, HRV_X_cubic, ...
+ HRV_Y_avefilt, HRV_Y_interp, HRV_Y_cubic, ...
+ HRV_Z_avefilt, HRV_Z_interp, HRV_Z_cubic] = preprocessHR(HR_data, HR_time, HRV_YZ, HRV_X, HRV_Y, HRV_Z, HRV_time);
 
 clean.EDA = horzcat(EDA_time,EDA_eurofilt);
 clean.TEMP = horzcat(TEMP_time,TEMP_expfilt);
 clean.HR = horzcat(HR_time,HR_cubic);
-clean.HRVZY = horzcat(HRV_time,HRV_ZY_cubic); 
+clean.HRVX = horzcat(HRV_time,HRV_X_cubic); 
+clean.HRVY = horzcat(HRV_time,HRV_Y_cubic); 
+clean.HRVZ = horzcat(HRV_time,HRV_Z_cubic); 
+clean.HRVYZ = horzcat(HRV_time,HRV_YZ_cubic); 
 
-% Save cleaned data
-save(strcat(OUT_DIR,'clean.mat'),'-struct','clean');
-
-%% Plot  
+% Plot  
 figure
-subplot(4,1,1)
+subplot(5,1,1)
 plot(unix_to_datetime(EDA_time),EDA_data(1:length(EDA_time)),'LineWidth',1)
 hold on;
 plot(unix_to_datetime(EDA_time),EDA_medfilt(1:length(EDA_time)),'LineWidth',1)
@@ -85,7 +92,7 @@ plot(unix_to_datetime(EDA_time), EDA_eurofilt(1:length(EDA_time)),'LineWidth',2)
 legend('raw','medfilt','avefilt','interp','eurofilt');
 ylabel("EDA (us)")
 
-subplot(4,1,2)
+subplot(5,1,2)
 plot(unix_to_datetime(TEMP_time),TEMP_data(1:length(TEMP_time)),'LineWidth',1)
 hold on
 plot(unix_to_datetime(TEMP_time),TEMP_medfilt(1:length(TEMP_time)),'LineWidth',1)
@@ -96,7 +103,7 @@ plot(unix_to_datetime(TEMP_time),TEMP_expfilt(1:length(TEMP_time)),'LineWidth',2
 ylabel("Temperature (C)")
 legend("raw", "medfilt", "avefilt", "expfilt")
 
-subplot(4,1,3)
+subplot(5,1,3)
 plot(unix_to_datetime(HR_time), HR_data(1:length(HR_time)),'LineWidth',1)
 hold on 
 plot(unix_to_datetime(HR_time), HR_avefilt(1:length(HR_time)),'LineWidth',1)
@@ -105,14 +112,27 @@ plot(unix_to_datetime(HR_time), HR_cubic(1:length(HR_time)),'LineWidth',2)
 legend("raw", "avefilt","cubic")
 ylabel("Heart rate")
 
-subplot(4,1,4)
-plot(unix_to_datetime(HRV_time), HRV_ZY(1:length(HRV_time)),'LineWidth',1)
+subplot(5,1,4)
+plot(unix_to_datetime(HRV_time), HRV_YZ(1:length(HRV_time)),'LineWidth',1)
 hold on 
-plot(unix_to_datetime(HRV_time), HRV_ZY_avefilt(1:length(HRV_time)),'LineWidth',1)
+plot(unix_to_datetime(HRV_time), HRV_YZ_avefilt(1:length(HRV_time)),'LineWidth',1)
 hold on 
-plot(unix_to_datetime(HRV_time), HRV_ZY_interp(1:length(HRV_time)),'LineWidth',1)
+plot(unix_to_datetime(HRV_time), HRV_YZ_interp(1:length(HRV_time)),'LineWidth',1)
 hold on
-plot(unix_to_datetime(HRV_time), HRV_ZY_cubic(1:length(HRV_time)),'LineWidth',2)
+plot(unix_to_datetime(HRV_time), HRV_YZ_cubic(1:length(HRV_time)),'LineWidth',2)
 legend("raw", "avefilt", "interp","cubicspline")
-ylabel("HRV Z/Y")
+ylabel("HRV Y/Z")
+
+subplot(5,1,5)
+plot(unix_to_datetime(HRV_time), HRV_Z(1:length(HRV_time)),'LineWidth',1)
+hold on 
+plot(unix_to_datetime(HRV_time), HRV_Z_avefilt(1:length(HRV_time)),'LineWidth',1)
+hold on 
+plot(unix_to_datetime(HRV_time), HRV_Z_interp(1:length(HRV_time)),'LineWidth',1)
+hold on
+plot(unix_to_datetime(HRV_time), HRV_Z_cubic(1:length(HRV_time)),'LineWidth',2)
+legend("raw", "avefilt", "interp","cubicspline")
+ylabel("HRV Z")
 xlabel("Time (seconds)")
+
+end 
--- Preprocessing/preprocessHR.m
+++ Preprocessing/preprocessHR.m
@@ -6,21 +6,36 @@
 %
 % ---------------------
 
-function [HR_avefilt, HR_cubic, HRV_ZY_avefilt, HRV_ZY_interp, HRV_ZY_cubic] = preprocessHR(HR_data, HR_time, HRV_ZY, HRV_time)
-
+function [HR_avefilt, HR_cubic, ...
+ HRV_YZ_avefilt, HRV_YZ_interp, HRV_YZ_cubic, ...
+ HRV_X_avefilt, HRV_X_interp, HRV_X_cubic, ...
+ HRV_Y_avefilt, HRV_Y_interp, HRV_Y_cubic, ...
+ HRV_Z_avefilt, HRV_Z_interp, HRV_Z_cubic] = preprocessHR(HR_data, HR_time, HRV_YZ, HRV_X, HRV_Y, HRV_Z, HRV_time)
 
 % Moving average filter 
 HR_avefilt = movmean(HR_data,5);
-HRV_ZY_avefilt = movmean(HRV_ZY,5);
+HRV_X_avefilt = movmean(HRV_X,5);
+HRV_Y_avefilt = movmean(HRV_Y,5);
+HRV_Z_avefilt = movmean(HRV_Z,5);
+HRV_YZ_avefilt = movmean(HRV_YZ,5);
 
 % If HRV is NaN shorter than 30 seconds interpolate with cubic spline 
-HRV_ZY_interp = interp1gap(HRV_ZY_avefilt,450,'pchip');
+HRV_X_interp = interp1gap(HRV_X_avefilt,450,'pchip');
+HRV_Y_interp = interp1gap(HRV_Y_avefilt,450,'pchip');
+HRV_Z_interp = interp1gap(HRV_Z_avefilt,450,'pchip');
+HRV_YZ_interp = interp1gap(HRV_YZ_avefilt,450,'pchip');
 
-% IF HRV is NaN longer than 30 seconds, interpolate with previous non NaN value value 
-HRV_ZY_interp = fillmissing(HRV_ZY_interp,'previous');
+% IF HRV is NaN longer than 30 seconds, interpolate with previous non NaN value value
+HRV_X_interp = fillmissing(HRV_X_interp,'previous');
+HRV_Y_interp = fillmissing(HRV_Y_interp,'previous');
+HRV_Z_interp = fillmissing(HRV_Z_interp,'previous');
+HRV_YZ_interp = fillmissing(HRV_YZ_interp,'previous');
 
 % Cubic splining function 
 HR_cubic = csaps(HR_time,HR_avefilt',0.001,HR_time); 
-HRV_ZY_cubic = csaps(HRV_time,HRV_ZY_interp',0.001,HRV_time);
+HRV_X_cubic = csaps(HRV_time,HRV_X_interp',0.001,HRV_time);
+HRV_Y_cubic = csaps(HRV_time,HRV_Y_interp',0.001,HRV_time);
+HRV_Z_cubic = csaps(HRV_time,HRV_Z_interp',0.001,HRV_time);
+HRV_YZ_cubic = csaps(HRV_time,HRV_YZ_interp',0.001,HRV_time);
 
 end 
