43202ec79d7c32ebbc45535c1f19d5bf25849d41
biomusic <dannie.fu.888@gmail.com>
2021-01-27 14:29:48 -0500

    Including HRV YZ and HRV Z into plots

--- Misc Scripts/plotClean.m
+++ Misc Scripts/plotClean.m
@@ -1,45 +1,69 @@
 figure
-subplot(4,1,1)
+subplot(5,1,1)
 plot(unix_to_datetime(EDA(:,1)),EDA(:,2),'LineWidth',1)
 ylabel("EDA (us)")
 set(gca,'FontSize',14)
 
-subplot(4,1,2)
+subplot(5,1,2)
 plot(unix_to_datetime(TEMP(:,1)),TEMP(:,2),'LineWidth',1)
 ylabel("Temperature (C)")
 set(gca,'FontSize',14)
 
-subplot(4,1,3)
+subplot(5,1,3)
 plot(unix_to_datetime(HR(:,1)), HR(:,2),'LineWidth',1)
 ylabel("Heart rate")
 set(gca,'FontSize',14)
 
-subplot(4,1,4)
-plot(unix_to_datetime(HRVZY(:,1)), HRVZY(:,2),'LineWidth',1)
-ylabel("HRV Z/Y")
+subplot(5,1,4) 
+plot(unix_to_datetime(HRVYZ(:,1)), HRVYZ(:,2),'LineWidth',1)
+ylabel("HRV Y/Z")
+set(gca,'FontSize',14)
+
+subplot(5,1,5)
+plot(unix_to_datetime(HRVZ(:,1)), HRVZ(:,2),'LineWidth',1)
+ylabel("HRV Z")
 xlabel("Time (seconds)")
 set(gca,'FontSize',14)
 
-% %% Plot window
-% 
-% figure
-% subplot(4,1,1)
-% plot(unix_to_datetime(EDA_window(:,1)),EDA_window(:,2),'LineWidth',1)
-% ylabel("EDA (us)")
-% 
-% subplot(4,1,2)
-% plot(unix_to_datetime(TEMP_window(:,1)),TEMP_window(:,2),'LineWidth',1)
-% ylabel("Temperature (C)")
-% 
-% subplot(4,1,3)
-% plot(unix_to_datetime(HR_window(:,1)), HR_window(:,2),'LineWidth',1)
-% ylabel("Heart rate")
-% 
-% subplot(4,1,4)
-% plot(unix_to_datetime(HRVZY_window(:,1)), HRVZY_window(:,2),'LineWidth',1)
-% ylabel("HRV Z/Y")
-% xlabel("Time (seconds)")
-% set(gca,'FontSize',14)
+%% Plot window
+
+figure
+subplot(5,1,1)
+plot(unix_to_datetime(EDA_window(:,1)),EDA_window(:,2),'LineWidth',1, 'Color', '#f6a753')
+ylabel("EDA (us)")
+set(gca,'FontSize',14)
+%set(gcf, 'color', 'none'); 
+%set(gca, 'color', 'none'); 
+
+subplot(5,1,2)  
+
+plot(unix_to_datetime(TEMP_window(:,1)),TEMP_window(:,2),'LineWidth',1,'Color', '#699bdd')
+ylabel("Temperature (C)")
+set(gca,'FontSize',14)
+%set(gcf, 'color', 'none'); 
+%set(gca, 'color', 'none'); 
+
+subplot(5,1,3)
+plot(unix_to_datetime(HR_window(:,1)), HR_window(:,2),'LineWidth',1, 'Color', '#94d169')
+ylabel("Heart rate")
+set(gca,'FontSize',14)
+%set(gcf, 'color', 'none'); 
+%set(gca, 'color', 'none'); 
+
+subplot(5,1,4)
+plot(unix_to_datetime(HRVYZ_window(:,1)), HRVYZ_window(:,2),'LineWidth',1, 'Color', '#94d169')
+ylabel("HRV Z/Y")
+set(gca,'FontSize',14)
+%set(gcf, 'color', 'none'); 
+%set(gca, 'color', 'none'); 
+
+subplot(5,1,5)
+plot(unix_to_datetime(HRVZ_window(:,1)), HRVZ_window(:,2),'LineWidth',1, 'Color', '#94d169')
+ylabel("HRV Z")
+xlabel("Time (seconds)")
+set(gca,'FontSize',14)
+%set(gcf, 'color', 'none'); 
+%set(gca, 'color', 'none'); 
 
 
 
