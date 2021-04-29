09a93ca898e17402879a7d5eb12ee4f3261a9399
biomusic <dannie.fu.888@gmail.com>
2021-04-29 11:15:36 -0400

    organizing preprocessing folder

--- 01_Preprocessing/preprocess_movingwith.m
+++ 01_Preprocessing/preprocess_movingwith.m
@@ -1,19 +1,24 @@
 % Dannie Fu June 2 2020
 %
-% This script preprocesses the moving with data (2019).
-% Must specify the load folder of a session.
-%
+% This script preprocesses EDA, TEMP, HR, and HRV from the Moving With data
+% (2019) and saves the cleaned signals in a struct called "clean.mat" in
+% the same folder as the load folder. 
+% 
+% Must change the "LOAD_DIR" variable, which specifies the load directory
+% of all the participants' data from a single Moving With Session.
+% 
 % ------------------
 
 clear;
 
-LOAD_DIR = "/Volumes/Seagate/Moving With 2019/7. Session_Dec_19/";
+LOAD_DIR = "/Volumes/Seagate/3. Session_Nov_14/";
 
 D = dir(LOAD_DIR);
 subfolders = setdiff({D([D.isdir]).name},{'.','..'}); % list of subfolders of D
     
 for s=1:length(subfolders)
     
+    % Skipping the folder with the original raw data
     if subfolders(s) == "Original Data"
         continue
     end 
@@ -31,18 +36,24 @@ for s=1:length(subfolders)
             filepath = strcat(LOAD_DIR,subfolders(s),'/',files(j).name);
             load(filepath);
         end 
-        preprocess;        
+        clean = preprocess(EDA,TEMP,HR,HRV); 
+        
+        save(strcat(OUT_DIR,"clean.mat"), "clean");
+        
     else % data is segmented 
         for k = 1:length(seg_folders)
             OUT_DIR = strcat(LOAD_DIR,subfolders(s),'/',char(seg_folders(k)),'/');
                 
             files = dir(strcat(LOAD_DIR, subfolders(s),'/', char(seg_folders(k)),'/*.mat'));
             
-             for a=1:length(files)
+            for a=1:length(files)
                 filepath = strcat(LOAD_DIR,subfolders(s),'/',char(seg_folders(k)),'/',files(a).name);
                 load(filepath);
-             end 
-             preprocess;
+            end 
+            clean = preprocess(EDA,TEMP,HR,HRV); 
+            
+            save(strcat(OUT_DIR,"clean.mat"), "clean");
+
         end         
-    end   
-end 
+    end
+end
